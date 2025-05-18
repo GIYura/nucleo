1b91cfb26ecc10c8396e67eb8bd83a57ba39190e
jura <jglotovyn@gmail.com>
2025-05-18 19:52:49 +0300

    ADD: pwm test

--- pwm/src/main.c
+++ pwm/src/main.c
@@ -1,7 +1,16 @@
 #include "pwm.h"
 
+static Pwm_t m_pwm = { .port = GPIOA, .pin = 9 };
+static PwmConfig_t m_pwmConfig = { .periodMs = 1000, .duty = 30 };
+
 int main(void)
 {
+    Pwm_Init(&m_pwm);
+
+    Pwm_Config(&m_pwmConfig);
+
+    Pwm_Start();
+
     while (1);
 
     return 0;
