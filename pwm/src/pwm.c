211ad845951c8d1545e60be444bd4941590ad78b
jura <jglotovyn@gmail.com>
2025-05-18 21:03:39 +0300

    ADD: min pwm functionality

--- pwm/src/pwm.c
+++ pwm/src/pwm.c
@@ -1,1 +1,50 @@
 #include "pwm.h"
+
+/*
+ * NOTE: Table 9. Alternate function mapping
+ * GPIO PA9 TIM1_CH2 AF01
+ * */
+
+void Pwm_Init(const Pwm_t* const pwm)
+{
+    /* Enable GPIOA and TIM1 clock */
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+    /* PA 9 AF mode */
+    pwm->port->MODER &= ~(3 << (pwm->pin * 2));
+    pwm->port->MODER |= (2 << (pwm->pin * 2));
+
+    /* GPIO alternate function AF1 */
+    pwm->port->AFR[1] &= ~(0xF << ((pwm->pin - 8) * 4));
+    pwm->port->AFR[1] |=  (1 << ((pwm->pin - 8) * 4));
+
+    TIM1->CCMR1 &= ~TIM_CCMR1_OC2M;
+    TIM1->CCMR1 |= (6 << TIM_CCMR1_OC2M_Pos);  /* PWM mode 1 */
+    TIM1->CCMR1 |= TIM_CCMR1_OC2PE;            /* Output Compare 2 preload enable */
+
+    TIM1->CCER |= TIM_CCER_CC2E;   /* Capture/Compare 2 output enable */
+
+    TIM1->BDTR |= TIM_BDTR_MOE;    /* Main output enable */
+    TIM1->CR1 |= TIM_CR1_ARPE;     /* Auto-reload preload enable */
+    TIM1->EGR |= TIM_EGR_UG;       /* Update generation */
+}
+
+void Pwm_Config(const PwmConfig_t* const config)
+{
+    /* TIM1 settings for PWM */
+    TIM1->PSC = 16 - 1;                 /* Prescaler: 16 МHz / 16 = 1 МHz */
+    TIM1->ARR = config->periodMs - 1;   /* Period: 1000 -> freq 1 кHz */
+
+    TIM1->CCR2 = (config->periodMs * config->duty) / 100;
+}
+
+void Pwm_Start(void)
+{
+    TIM1->CR1 |= TIM_CR1_CEN;
+}
+
+void Pwm_Stop(void)
+{
+    TIM1->CR1 &= ~TIM_CR1_CEN;
+}
--- pwm/src/pwm.h
+++ pwm/src/pwm.h
@@ -1,4 +1,23 @@
 #ifndef PWM_H
 #define PWM_H
 
+#include "stm32f411xe.h"
+
+typedef struct
+{
+    GPIO_TypeDef* port;
+    uint8_t pin;
+} Pwm_t;
+
+typedef struct
+{
+    uint32_t periodMs;
+    uint32_t duty;
+} PwmConfig_t;
+
+void Pwm_Init(const Pwm_t* const pwm);
+void Pwm_Config(const PwmConfig_t* const config);
+void Pwm_Start(void);
+void Pwm_Stop(void);
+
 #endif /* PWM_H */
