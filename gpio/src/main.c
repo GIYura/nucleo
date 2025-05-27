6a039cb69050286ed60d6b3e49dfcfa0489dfed9
jura <jglotovyn@gmail.com>
2025-05-27 21:03:28 +0300

    ADD: test for leds and button

--- gpio/src/main.c
+++ gpio/src/main.c
@@ -1,8 +1,19 @@
 #include "gpio.h"
 
+typedef enum
+{
+    GREEN_INT = 0,
+    GREEN_EXT,
+    YELLOW,
+    WHITE,
+    LED_NUMBER
+} LED_COLORS;
+
 volatile static uint32_t m_ticks = 0;
 
-static Gpio_t m_led;
+static Gpio_t m_leds[LED_NUMBER];
+static Gpio_t m_button;
+static uint32_t m_buttonState;
 
 void SysTick_Handler(void)
 {
@@ -20,12 +31,23 @@ int main(void)
 {
     SysTick_Config(SystemCoreClock / 1000);
 
-    GpioInit(&m_led, PA_5, PIN_MODE_OUTPUT, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_STATE_LOW);
+    GpioInit(&m_leds[GREEN_INT], PA_5, PIN_MODE_OUTPUT, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_STATE_HIGH);
+    GpioInit(&m_leds[GREEN_EXT], PA_0, PIN_MODE_OUTPUT, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_STATE_HIGH);
+    GpioInit(&m_leds[YELLOW], PC_3, PIN_MODE_OUTPUT, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_STATE_HIGH);
+    GpioInit(&m_leds[WHITE], PB_13, PIN_MODE_OUTPUT, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_STATE_HIGH);
+
+    GpioInit(&m_button, PA_1, PIN_MODE_INPUT, PIN_TYPE_PULL_UP, PIN_SPEED_HIGH, PIN_CONFIG_PUSH_PULL, 0);
 
     while (1)
     {
-        GpioToogle(&m_led);
-        DelayMs(200);
+        for (uint8_t i = 0; i < LED_NUMBER; i++)
+        {
+            GpioToogle(&m_leds[i]);
+        }
+
+        m_buttonState = GpioRead(&m_button);
+
+        DelayMs(250);
     }
 
     return 0;
