b640698db66324d026db5ea1512faa37a31f575a
jura <jglotovyn@gmail.com>
2025-06-25 18:06:27 +0300

    UPDATE: uart driver test

--- serial/src/main.c
+++ serial/src/main.c
@@ -1,12 +1,12 @@
+#include <string.h>
+
 #include "logger.h"
 #include "delay.h"
 #include "uart.h"
 
-static char m_messageUart1[] = "Hello UART-1\r\n";
-static char m_messageUart6[] = "Hello UART-6\r\n";
+static char m_messageUart1[] = "Hello STM32F411 Nucleo board over UART-1\r\n";
 
 static Uart_t m_uart1;
-static Uart_t m_uart6;
 
 int main(void)
 {
@@ -21,14 +21,10 @@ int main(void)
 #endif
 
     UartInit(&m_uart1, UART_1, BAUD_921600);
-    UartInit(&m_uart6, UART_6, BAUD_115200);
 
     while (1)
     {
-        UartWrite(&m_uart1, m_messageUart1);
-        UartWrite(&m_uart6, m_messageUart6);
-
-        DelayMs(1000);
+        UartWrite(&m_uart1, (uint8_t*)m_messageUart1, strlen(m_messageUart1));
     }
 
     return 0;
