4e6673b976dddf5e289ccbdab1419a3fc44f2d6a
jura <jglotovyn@gmail.com>
2025-06-30 21:58:53 +0300

    ADD: logger test

--- serial/src/main.c
+++ serial/src/main.c
@@ -1,34 +1,16 @@
-#include <string.h>
-
 #include "logger.h"
-#include "delay.h"
-#include "uart.h"
-
-static char m_messageUart1[] = "Hello STM32F411 Nucleo board over UART-1\r\n";
-static char m_messageUart6[] = "Hello STM32F411 Nucleo board over UART-6\r\n";
-
-static Uart_t m_uart1;
-static Uart_t m_uart6;
 
 int main(void)
 {
-#if 0
     LogInit();
-    LogSetLevel(LOG_LEVEL_DEBUG);
-
-    LogDebug("Debug message\r\n");
-    LogInfo("Info message\r\n");
-    LogWarn("Warn message\r\n");
-    LogError("Error message\r\n");
-#endif
-
-    UartInit(&m_uart1, UART_1, BAUD_921600);
-    UartInit(&m_uart6, UART_6, BAUD_921600);
+    LogSetLevel(LOG_LEVEL_ERROR);
 
     while (1)
     {
-        UartWrite(&m_uart1, (uint8_t*)m_messageUart1, strlen(m_messageUart1));
-        UartWrite(&m_uart6, (uint8_t*)m_messageUart6, strlen(m_messageUart6));
+        LogDebug("Debug message");
+        LogInfo("Info message");
+        LogWarn("Warning message");
+        LogError("Error message");
     }
 
     return 0;
