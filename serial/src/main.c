28d11e29c9943f8aca3710182012571e00ed3350
jura <jglotovyn@gmail.com>
2025-07-01 21:49:20 +0300

    ADD: test for log dec, hex

--- serial/src/main.c
+++ serial/src/main.c
@@ -11,6 +11,11 @@ int main(void)
         LogInfo("Info message");
         LogWarn("Warning message");
         LogError("Error message");
+        LogHex(22);
+        LogHex(0x22);
+        LogHex(0xDEADBEEF);
+        LogDec(1234);
+        LogDec(-1234);
     }
 
     return 0;
