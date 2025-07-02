ec7287e97996e9a57f3da231736d2b9076d1fec0
jura <jglotovyn@gmail.com>
2025-07-02 19:19:14 +0300

    ADD: test for formatted output

--- serial/src/main.c
+++ serial/src/main.c
@@ -2,20 +2,11 @@
 
 int main(void)
 {
-    LogInit();
-    LogSetLevel(LOG_LEVEL_ERROR);
+    LogInit(LOG_LEVEL_ERROR);
 
     while (1)
     {
-        LogDebug("Debug message");
-        LogInfo("Info message");
-        LogWarn("Warning message");
-        LogError("Error message");
-        LogHex(22);
-        LogHex(0x22);
-        LogHex(0xDEADBEEF);
-        LogDec(1234);
-        LogDec(-1234);
+        LogPrint("%d %u %x %c %s", -15, 17, 10, 'A', "FAIL");
     }
 
     return 0;
