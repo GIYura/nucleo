34b4c369216afe0280b1c186d39a819f13984770
jura <jglotovyn@gmail.com>
2025-10-10 15:32:55 +0300

    ADD: toggle led on button press

--- button-os/src/main.c
+++ button-os/src/main.c
@@ -1,18 +1,19 @@
 #include <stdint.h>
 
 #include "button.h"
+#include "led.h"
+
 #include "FreeRTOS.h"
 #include "task.h"
 
 static Button_t m_button;
-
-static uint32_t m_buttonPressCounter = 0;
+static Led_t m_ledGreen;
 
 static volatile uint32_t m_idleCycleCount = 0;
 
 static void OnButton(void)
 {
-    ++m_buttonPressCounter;
+    LedToggle(&m_ledGreen);
 }
 
 int main(void)
@@ -20,6 +21,9 @@ int main(void)
     if (ButtonInit(&m_button, PC_13))
     {
         ButtonRegisterPressHandler(OnButton);
+
+        LedInit(&m_ledGreen, PA_0);
+
         vTaskStartScheduler();
     }
 
