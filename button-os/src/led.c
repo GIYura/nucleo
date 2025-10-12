e1ee248f517e3a51193450221bf2c563c1e47b8c
jura <jglotovyn@gmail.com>
2025-10-12 21:29:30 +0300

    ADD: led

--- button-os/src/led.c
+++ button-os/src/led.c
@@ -3,45 +3,30 @@
 #include "assert.h"
 #include "led.h"
 
-/* array of gpios to control LEDs */
-static Gpio_t m_ledGpio[LED_MAX];
-
-/* gpio index */
-static uint8_t m_ledGpioIndex = 0;
-
-uint8_t LedInit(Led_t* const led, PIN_NAMES pinName)
+void LedInit(Led_t* const led, PIN_NAMES pinName)
 {
     ASSERT(led != NULL);
 
-    if (m_ledGpioIndex >= LED_MAX)
-    {
-        return 1;
-    }
-
-    led->gpio = &m_ledGpio[m_ledGpioIndex++];
-
-    GpioInit(led->gpio, pinName, PIN_MODE_OUTPUT, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_STATE_LOW);
-
-    return 0;
+    GpioInit(&led->gpio, pinName, PIN_MODE_OUTPUT, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_STATE_LOW);
 }
 
 void LedOn(const Led_t* const led)
 {
     ASSERT(led != NULL);
 
-    GpioWrite(led->gpio, PIN_STATE_HIGH);
+    GpioWrite(&led->gpio, PIN_STATE_HIGH);
 }
 
 void LedOff(const Led_t* const led)
 {
     ASSERT(led != NULL);
 
-    GpioWrite(led->gpio, PIN_STATE_LOW);
+    GpioWrite(&led->gpio, PIN_STATE_LOW);
 }
 
 void LedToggle(const Led_t* const led)
 {
     ASSERT(led != NULL);
 
-    GpioToggle(led->gpio);
+    GpioToggle(&led->gpio);
 }
--- button-os/src/led.h
+++ button-os/src/led.h
@@ -6,25 +6,18 @@
 #include "gpio-name.h"
 #include "gpio.h"
 
-#if 0
-Current implementation support 4 LEDs. Gpio pins for LEDs statically allocated in led.c.
-This implemantation can be improved by passing gpio object from the caller.
-#endif
-
-#define LED_MAX     4   /* max available LEDs in the system */
-
 /* LED definition */
 typedef struct
 {
-    Gpio_t* gpio;
+    Gpio_t gpio;
 } Led_t;
 
 /*Brief: LED initialization
  * [in] - led - pointer to LED object
  * [in] - pinName - name of the pin defined in gpio-name.h
- * [out] - 0 - success; 1 - fail (run out of available gpio)
+ * [out] - none
  * */
-uint8_t LedInit(Led_t* const led, PIN_NAMES pinName);
+void LedInit(Led_t* const led, PIN_NAMES pinName);
 
 /*Brief: LED On
  * [in] - led - pointer to LED object
